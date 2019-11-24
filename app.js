4e7e8527433fc227444f5e5051a3e82598a9977d
Edd Tubbs <ed@silesiacoin.org>
2019-11-24 18:47:57 +0100

    Clear and add self-documented endpoint plus create account function

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
 const express = require('express');
 const Web3 = require('web3');
 const Accounts = require('web3-eth-accounts');
-
-const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
-
 const app = express();
-const port = 8081;
+const port = (process.env.port || 8081);
 
-app.get('/accounts', async(req, res) => res.json(await getAccountList()));
-app.get('/accounts/unlock', async(req, res) => res.json(await unlockAccount()));
-app.get('/accounts/create', async(req, res) => res.json(await createAccount()));
-app.get('/provider', (req, res) => res.json(getCurrentProvider()));
+app.get('/', (req, res) => res.json(res.json(indexResponse())));
+app.put('/accounts', async(req, res) => res.json(await createAccount()));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Authenticator listening on port ${port}!`));
 
 
-const getCurrentProvider = () => {
-    return { body: { version: web3.version, provider: web3.currentProvider }};
-};
-
-const getAccountList = async() => {
-    const list = await web3.eth.getAccounts();
-
-    return { accounts: list }
+const indexResponse = () => {
+    return {
+        routes: app._router.stack
+            .filter(possibleRoute => possibleRoute.route)
+            .map(possibleRoute => {
+                return {
+                    route: possibleRoute.route.path,
+                    methods: possibleRoute.route.methods
+                }
+            })
+    }
 };
 
 const createAccount = async() => {
@@ -35,30 +33,4 @@ const createAccount = async() => {
         address,
         privateKey
     }
-};
-
-const unlockAccount = async() => {
-    const accounts = await web3.eth.personal.getAccounts();
-    let accountsResponse = {};
-
-    if (!Array.isArray(accounts) && accounts.length < 0) {
-        return;
-    }
-
-    const parseAccounts = async () => {
-        for (const account of accounts) {
-            const unlocked = await web3.eth.personal.unlockAccount(
-                accounts[0],
-                'EloMelo1!',
-                20
-            );
-            accountsResponse[account] = { unlocked: unlocked }
-        }
-    };
-
-    await parseAccounts();
-
-    console.log(accountsResponse);
-
-    return accountsResponse;
 };
